07ed16dd1bad6462d98ef015a0829edf9da828f5
Niveditha <nivedithak@99x.lk>
2019-10-30 09:47:44 +0530

    Add base button component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
+import Button from './Components/Button/button';
 
 export default class extends Component {
   render() {
     return <div>
-      <h2>Welcome to React components</h2>
+      <h2>Welcome to React UI components</h2>
+
+      <h4>Basic components</h4>
+      <label>Button: </label><Button text="Submit"></Button>
     </div>
   }
 }
